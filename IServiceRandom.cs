9db96c9bafdd77d2141ab54d0c78f447de750426
fabianmedrano <135065707+fabianmedrano@users.noreply.github.com>
2023-09-04 10:26:19 -0600

    Agregar archivos de proyecto.

---
+++ IServiceRandom.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WcfServiceLibrary
+{
+    [ServiceContract]
+    public interface IServiceRandom
+    {
+        [OperationContract]
+         string GetRandom();
+    }
+}
---
+++ ServiceRandom.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WcfServiceLibrary
+{
+
+    public class ServiceRandom :IServiceRandom
+    {
+     
+        public string GetRandom()
+        {
+            Random rnd = new Random();
+            int randomNumber = rnd.Next(0, 101);
+            return string.Format("Your random Number: {0}", randomNumber);
+        }
+    }
+}
